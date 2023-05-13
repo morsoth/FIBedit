cb4e002356cf82df3950beb0eb93a8518538ccd6
Pau <morsoth@pop-os.localdomain>
2023-05-13 23:20:07 +0200

    a

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 #include <QApplication>
 #include <QFile>
 #include "MyForm.h"
-#include "style.h"
+//#include "style.h"
 
 int main(int argc, char **argv) {
     QApplication app(argc, argv);
 
-    Style::addTheme("default", { "rgb(209, 209, 209)", "rgb(36, 36, 36)", "rgb(45, 45, 45)", "rgb(60, 60, 60)", "rgb(250, 210, 60)"});
-    Style::fontSize = 14;
-    Style::fontName = "";
+    //Style::addTheme("default", { "rgb(209, 209, 209)", "rgb(36, 36, 36)", "rgb(45, 45, 45)", "rgb(60, 60, 60)", "rgb(250, 210, 60)"});
+    //Style::fontSize = 14;
+    //Style::fontName = "";
 
-    app.setStyleSheet(Style::selectTheme("default"));
+    //app.setStyleSheet(Style::selectTheme("default"));
 
     MyForm form;
     form.show();
